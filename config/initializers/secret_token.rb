acc0a38663cd1e19cfba4d2c5da387159908d633
Niki Liu <nishiki.liu@gmail.com>
2013-09-25 16:43:21 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Transferur::Application.config.secret_key_base = 'c6e734e104b6e49fed4f91484212d5ea01046e3aac623a389d23421d4cc2ba1942873a2cea82896ebbf5631388652084a0c3aa4d3f51793df2ceb484d14faa8a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Transferur::Application.config.session_store :cookie_store, key: '_transferur_session'
