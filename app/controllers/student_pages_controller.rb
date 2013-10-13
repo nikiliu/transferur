3519e76cb5a261820e51ac21606469aaf590d17f
Niki Liu <nishiki.liu@gmail.com>
2013-10-13 17:28:45 -0400

    Minor refactoring, basic form functionality

--- app/controllers/student_pages_controller.rb
+++ app/controllers/student_pages_controller.rb
@@ -12,6 +12,29 @@ class StudentPagesController < ApplicationController
     render partial: "transfer_courses"
   end
 
+  # Check whether a transfer request can be approved
   def new_transfer_request
+
+    # Parse form data
+    student_name       = params[:name]
+    student_email      = params[:email]
+    transfer_school_id = params[:transfer_school]
+    transfer_course_id = params[:transfer_course]
+    ur_course_id       = params[:course_number]
+
+    # Search database for an existing transfer request based on form data
+    query = TransferRequest.find_by(transfer_school_id: transfer_school_id,
+                                    transfer_course_id: transfer_course_id,
+                                    ur_course_id:       ur_course_id)
+
+    # Display output corresponding to whether the transfer request is approved,
+    # not approved, or pending.
+    if query != nil
+      @pending  = false
+      @approved = query.approved
+      @reasons  = query.reasons
+    else
+      @pending = true
+    end
   end
 end
