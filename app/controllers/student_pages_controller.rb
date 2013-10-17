fcdb47de42c89fda27900797f98fadffe9af9f4b
Niki Liu <nishiki.liu@gmail.com>
2013-10-16 21:17:35 -0400

    Student form integration tests

--- app/controllers/student_pages_controller.rb
+++ app/controllers/student_pages_controller.rb
@@ -3,7 +3,7 @@ class StudentPagesController < ApplicationController
   # Handles homepage
   def home
     @transfer_schools = School.where.not(id: 1).order(:name)
-    @ur_courses       = School.first.courses.all
+    @ur_courses       = School.first.courses
   end
 
   # Returns a label and select corresponding to the given school_id
@@ -20,26 +20,39 @@ class StudentPagesController < ApplicationController
     student_email      = params[:email]
     transfer_school_id = params[:transfer_school]
     transfer_course_id = params[:transfer_course]
-    transfer_online    = params[:online]
-    transfer_dual      = params[:dual_enrollment]
+    transfer_online    = params[:online] == "1"            # convert to boolean
+    transfer_dual      = params[:dual_enrollment] == "1"   # convert to boolean
     ur_course_id       = params[:course_number]
+
+    # Validate name and email fields
+    if student_name.empty?
+      flash[:error] = "Please enter a name."
+      redirect_to root_path
+      return
+    elsif student_email.empty? or not student_email =~ /@/
+      flash[:error] = "Please enter a valid email address."
+      redirect_to root_path
+      return
+    end
 
     # Search database for an existing transfer request based on form data
     query = TransferRequest.find_by(transfer_school_id: transfer_school_id,
                                     transfer_course_id: transfer_course_id,
                                     ur_course_id:       ur_course_id)
 
-    # Display output corresponding to whether the transfer request is approved,
-    # not approved, or pending.
-    @pending = true
-    if transfer_online
-      @pending  = false
-      @approved = false
-      @reasons  = "Online courses not accepted"
-    elsif query != nil and query.updated_at >= 5.years.ago and not transfer_dual
-      @pending  = false
-      @approved = query.approved
-      @reasons  = query.reasons
+    # Handle cases for submitted form data
+    if query != nil and query.updated_at >= 5.years.ago and not transfer_dual
+      flash[:success] = "Your transfer request was successfully submitted. Please check " +
+                        "your email to see if your request was approved."
+    elsif transfer_online
+      flash[:success] = "Your transfer request for an online course was successfully submitted. " +
+                        "Please check your email to see if your request was approved."
+    else
+      flash[:pending] = "Your transfer request is currently pending. An email notification " +
+                        "will be sent once your request is approved or not approved."
     end
+
+    # Redirect back to main form
+    redirect_to root_path
   end
 end
---
+++ test/integration/student_form_test.rb
@@ -0,0 +1,63 @@
+require 'test_helper'
+
+class StudentFormTest < ActionDispatch::IntegrationTest
+  def setup
+    @school1 = School.create!(name: "Foo University", location: "Foo", international: false)
+    @school1.courses.create!(name: "Calculus I",  course_num: "MATH 121")
+    @school1.courses.create!(name: "Calculus II", course_num: "MATH 122")
+
+    @school2 = School.create!(name: "Bar University", location: "Bar", international: true)
+    @school2.courses.create!(name: "Calculus I",  course_num: "MATH 101")
+    @school2.courses.create!(name: "Calculus II", course_num: "Math 102")
+
+    @transfer1 = TransferRequest.create!(transfer_school_id: @school1.id,
+                                         transfer_course_id: @school1.courses.first.id,
+                                         ur_course_id:       @school2.courses.first.id,
+                                         approved:           true)
+
+    @form_data = {
+      name:            "John Doe",
+      email:           "test@test.com",
+      transfer_school: @school1.id,
+      transfer_course: @school1.courses.first.id,
+      online:          0,
+      dual_enrollment: 0,
+      course_number:   @school2.courses.first.id
+    }
+  end
+
+  def teardown
+    @transfer1.destroy!
+    @school1.destroy!
+    @school2.destroy!
+  end
+
+  test "successful transfer request" do
+    post_via_redirect new_transfer_request_path, ActionController::Parameters.new(@form_data)
+    assert !flash[:success].nil?
+  end
+
+  test "dual enrollment transfer request" do
+    @form_data[:dual_enrollment] = 1
+    post_via_redirect new_transfer_request_path, ActionController::Parameters.new(@form_data)
+    assert !flash[:pending].nil?
+  end
+
+  test "missing name" do
+    @form_data[:name] = ""
+    post_via_redirect new_transfer_request_path, ActionController::Parameters.new(@form_data)
+    assert !flash[:error].nil?
+  end
+
+  test "missing email" do
+    @form_data[:email] = ""
+    post_via_redirect new_transfer_request_path, ActionController::Parameters.new(@form_data)
+    assert !flash[:error].nil?
+  end
+
+  test "invalid email" do
+    @form_data[:email] = "foo"
+    post_via_redirect new_transfer_request_path, ActionController::Parameters.new(@form_data)
+    assert !flash[:error].nil?
+  end
+end
