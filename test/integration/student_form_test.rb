bf108187947840bba70b068ebc4026c789c725f2
Niki Liu <nishiki.liu@gmail.com>
2013-10-17 11:55:04 -0400

    Fix failing tests

--- test/integration/student_form_test.rb
+++ test/integration/student_form_test.rb
@@ -2,27 +2,27 @@ require 'test_helper'
 
 class StudentFormTest < ActionDispatch::IntegrationTest
   def setup
-    @school1 = School.create!(name: "Foo University", location: "Foo", international: false)
-    @school1.courses.create!(name: "Calculus I",  course_num: "MATH 121")
-    @school1.courses.create!(name: "Calculus II", course_num: "MATH 122")
+    @school1 = School.create!(name: "University of Richmond", location: "Richmond, VA", international: false)
+    @school1.courses.create!(name: "Calculus I",  course_num: "MATH 211")
+    @school1.courses.create!(name: "Calculus II", course_num: "MATH 212")
 
     @school2 = School.create!(name: "Bar University", location: "Bar", international: true)
     @school2.courses.create!(name: "Calculus I",  course_num: "MATH 101")
     @school2.courses.create!(name: "Calculus II", course_num: "Math 102")
 
-    @transfer1 = TransferRequest.create!(transfer_school_id: @school1.id,
-                                         transfer_course_id: @school1.courses.first.id,
-                                         ur_course_id:       @school2.courses.first.id,
+    @transfer1 = TransferRequest.create!(transfer_school_id: @school2.id,
+                                         transfer_course_id: @school2.courses.first.id,
+                                         ur_course_id:       @school1.courses.first.id,
                                          approved:           true)
 
     @form_data = {
       name:            "John Doe",
       email:           "test@test.com",
-      transfer_school: @school1.id,
-      transfer_course: @school1.courses.first.id,
+      transfer_school: @school2.id,
+      transfer_course: @school2.courses.first.id,
       online:          0,
       dual_enrollment: 0,
-      course_number:   @school2.courses.first.id
+      course_number:   @school1.courses.first.id
     }
   end
 
