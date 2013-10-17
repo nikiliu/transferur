865b4ec4e30022d4e07b0889572a75a940ea7cff
Niki Liu <nishiki.liu@gmail.com>
2013-10-17 16:32:17 -0400

    Minor spacing fix in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Transferur::Application.routes.draw do
   match "/new_transfer_request",    to: "student_pages#new_transfer_request",    via: "post"
 
   devise_for :users, path: "",
-                     path_names: { sign_in:  "login", sign_out: "logout" },
+                     path_names: { sign_in: "login", sign_out: "logout" },
                      skip: :registrations
   as :user do
     get "admin/user" => "devise/registrations#edit",   as: "edit_user_registration"
