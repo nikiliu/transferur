cfb4702d7332b2f5ee852267c856ef8f87b3f822
Niki Liu <nishiki.liu@gmail.com>
2013-10-28 21:59:35 -0400

    Fixed conditionals in results mailer that caused wonky display of course information in results emails

--- app/mailers/results_mailer.rb
+++ app/mailers/results_mailer.rb
@@ -2,12 +2,18 @@ class ResultsMailer < ActionMailer::Base
   default from: "noreply@richmond.edu"
 
   def result_email(params, options)
-    @request         = params[:pending_request]
-    @other_school    = (@request[:transfer_school_other] == "1" or @request[:transfer_school_other])
-    @other_course    = (@request[:transfer_course_other] == "1" or @request[:transfer_course_other])
-    @dual_enrollment = (@request[:dual_enrollment]       == "1" or @request[:dual_enrollment])
-    @online          = (params[:online]                  == "1")
-    @options         = options
+    @request = params[:pending_request]
+    @online  = (params[:online] == "1")
+    @options = options
+
+    # The following values check for equivalence with "1" in the case that the passed
+    # params are directly from the live form, and with true in the case that they are
+    # directly from a pending request.
+    @other_school    = (@request[:transfer_school_other] == "1" or @request[:transfer_school_other] == true)
+    @other_course    = (@request[:transfer_course_other] == "1" or @request[:transfer_course_other] == true)
+    @dual_enrollment = (@request[:dual_enrollment]       == "1" or @request[:dual_enrollment]       == true)
+
+    # Return mail object
     mail(to: @request[:requester_email], subject: "UR Math Course Transfer Request")
   end
 end
